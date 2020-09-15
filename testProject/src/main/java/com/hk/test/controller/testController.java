6b29df361c19776ab91c695a3498a7fedec1b55d
hk_edu <hk_edu@192.168.4.121>
2020-09-15 17:20:17 +0900

    Controller 최종본

--- testProject/src/main/java/com/hk/test/controller/testController.java
+++ testProject/src/main/java/com/hk/test/controller/testController.java
@@ -1,13 +1,28 @@
 package com.hk.test.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.hk.test.dto.MemberDto;
+import com.hk.test.service.TestSerivce;
 
 @Controller
 public class testController {
 
-	@GetMapping("/")
-	public String root() { 
-		return "aaa";
+	@Autowired
+	TestSerivce testService;
+	
+	@GetMapping("/register")
+	public String registerGet(Model model) { 
+		return "registerGet";
+	}
+	
+	@PostMapping("/register")
+	public String registerPost(Model model, MemberDto memberDto) {
+		model.addAttribute("result", testService.testRegister(memberDto));
+		return "registerPost";
 	}
 }
