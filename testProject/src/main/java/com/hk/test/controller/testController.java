151b469127d451fe779d94b88f1a56e6cf506672
hk_edu <hk_edu@192.168.4.121>
2020-09-15 17:08:27 +0900

    333

--- testProject/src/main/java/com/hk/test/controller/testController.java
+++ testProject/src/main/java/com/hk/test/controller/testController.java
@@ -1,6 +1,7 @@
 package com.hk.test.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class testController {
